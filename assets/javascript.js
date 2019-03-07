9e4923e871923025d674c025b68704aa73fcae8a
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-03-07 09:55:29 -0500

    test

---
+++ assets/javascript.js
@@ -0,0 +1,35 @@
+$(document).ready()
+
+let countDown;
+let correctAnswers = 0;
+let inccorectAnswers = 0;
+let notAnswered = 0;
+
+let questions = {
+    
+}
+
+
+// Click Start button to begin  game
+
+
+
+
+//Once start button is clicked counter begins to count down (120 ish secs)
+
+
+
+
+//Object that contains trivia questions, answer choices, correct answers
+
+
+
+//Need a function that sorts and stores correct answers
+
+
+
+// When counter equals zero then the game ends and the follow is displayed
+    // All DONE
+    // Correct Answers:
+    // Incorrect Ansers:
+    // Unanswered:
